68c9bcc7648889f6888033b713eea78b6cb533d6
Ian Good <ian.good@rackspace.com>
2012-04-01 05:47:15 +0000

    updates to client

--- test/smtp-client-test.js
+++ test/smtp-client-test.js
@@ -2,7 +2,9 @@
 var vows = require("vows"),
     assert = require("assert");
 
-var client = require("../lib/smtp/client");
+var client = require("../lib/smtp/client"),
+    commands = client.commands,
+    replies = client.replies;
 
 vows.describe("smtp client").addBatch({
     'exports': {
@@ -12,16 +14,16 @@ vows.describe("smtp client").addBatch({
             assert.ok(topic.Client);
         },
 
-        'the connect function': function (topic) {
-            assert.ok(topic.connect);
+        'the commands module': function (topic) {
+            assert.equal(topic.commands, require("../lib/smtp/commands"));
         },
+
+        'the replies module': function (topic) {
+            assert.equal(topic.replies, require("../lib/smtp/replies"));
+        },
+
     },
 
-    'the Client constructor': {
-        'with default options': {
-            topic: new client.Client(),
-        },
-    },
 }).export(module);
 
 // vim:et:sw=4:ts=4:sts=4:

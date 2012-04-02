112edc98627e6ceb134aae2d16201780d2955756
Ian Good <ian.good@rackspace.com>
2012-04-02 03:17:29 +0000

    build/parse extensions test

--- test/smtp-extensions-test.js
+++ test/smtp-extensions-test.js
@@ -67,7 +67,87 @@ vows.describe("smtp extensions").addBatch({
                 },
             },
         },
+    },
 
+    'the parseEhloString function': {
+        'given no complete lines': {
+            topic: extensions.parseEhloString("testing"),
+
+            'the only line is a header': function (topic) {
+                assert.equal(topic.header, "testing");
+            },
+
+            'it still gives an EsmtpExtensions object': function (topic) {
+                assert.ok(topic.extensions instanceof extensions.EsmtpExtensions);
+            },
+        },
+
+        'given a complete line and an unparseable extensions': {
+            topic: extensions.parseEhloString("testing\r\n!@#"),
+
+            'the header is the first line': function (topic) {
+                assert.equal(topic.header, "testing");
+            },
+
+            'the second line is not an extension': function (topic) {
+                assert.ok(!topic.extensions.has("!@#"));
+            },
+        },
+
+        'given a complete line and some extensions': {
+            topic: extensions.parseEhloString("testing header\r\n8bitmime\r\npipelining\r\nauth plain login\r\nsize 1234"),
+
+            'the header is the first line': function (topic) {
+                assert.equal(topic.header, "testing header");
+            },
+
+            'adds the extensions without parameters': function (topic) {
+                assert.ok(topic.extensions.has("8BITMIME"));
+                assert.ok(topic.extensions.has("PIPELINING"));
+            },
+
+            'adds the extensions with parameters': function (topic) {
+                assert.equal(topic.extensions.has("AUTH"), "plain login");
+                assert.equal(topic.extensions.has("SIZE"), "1234");
+            },
+        },
+    },
+
+    'the buildEhloString function': {
+        'given an EsmtpExtensions object with no extensions': {
+            topic: function () {
+                var ext = new extensions.EsmtpExtensions();
+                return extensions.buildEhloString(ext, "testing");
+            },
+
+            'the result is correct': function (topic) {
+                assert.equal(topic, "testing");
+            },
+        },
+
+        'given an EsmtpExtensions object with an extension with no parameter': {
+            topic: function () {
+                var ext = new extensions.EsmtpExtensions();
+                ext.add("STARTTLS");
+                return extensions.buildEhloString(ext, "testing header");
+            },
+
+            'the result is correct': function (topic) {
+                assert.equal(topic, "testing header\r\nSTARTTLS");
+            },
+        },
+
+        'given an EsmtpExtensions object with an extension with a parameter': {
+            topic: function () {
+                var ext = new extensions.EsmtpExtensions();
+                ext.add("auth", "plain LOGIN");
+                return extensions.buildEhloString(ext, "testing header");
+            },
+
+            'the result is correct': function (topic) {
+                assert.equal(topic, "testing header\r\nAUTH plain LOGIN");
+            },
+        },
     },
 }).export(module);
 
